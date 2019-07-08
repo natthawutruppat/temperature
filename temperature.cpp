00ef94b670df38975feb80f9e267705c82689ce0
Natthawut Ruppat <52170322+natthawutruppat@users.noreply.github.com>
2019-07-08 18:51:26 +0700

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+void main()
+
+{
+ 
+float cc , frh ; 
+cout << ("Enter Celsius temperature : ") ;
+cin >> cc ; 
+
+frh =  (1.8 * cc) + 32 ;
+cout << "  " << endl; 
+cout << "Fahrenheit = " << frh << endl;
+cout << "  " << endl; 
+cout << frh << " is "<<  (frh > 70 ?"Now weather in Thailand is HOT":"Now weather in Thailand is COOL" ) ;
+cout << "  " << endl; 
+
+}
+
